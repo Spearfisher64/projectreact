fcbad5d67e6f7cf925f6fe883cb13753a81f8e0d
Spear <ege99olgun@gmail.com>
2021-04-05 03:56:43 +0300

    Mistake Fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useReducer,useContext, useEffect} from 'react';
 
-function App() {
+function appReducer(state,action){
+  switch(action.type){
+    case 'reset':{
+      return action.payload;
+    }
+    case 'add': {
+      return [
+
+        ...state,{
+          id: Date.now(),
+          text: '',
+          completed: false,
+        }
+      ]
+    }
+    case 'delete':{
+      return state.filter(item => item.id !== action.payload);
+    }
+    case 'completed':{
+      return state.map(item=> {
+        if(item.id === action.payload){
+          return{
+            ...item,
+            completed: !item.completed
+          }
+        }
+        return item;         
+      })
+    }
+     default:{
+       return state;
+     }
+  }
+}
+const Context = React.createContext();
+
+
+export default function TodosApp() {
+const [state,dispatch] = useReducer(appReducer, []);
+useEffect(() => {
+  const raw = localStorage.getItem('data');
+  dispatch({ type: 'reset',payload: JSON.parse(raw)})
+},[])
+
+useEffect(()=>{
+  localStorage.setItem('data', JSON.stringify(state));
+},
+[state]
+ )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Context.Provider value={dispatch}>
+    <h1>      Todo App    </h1>
+
+    <button onClick={() => dispatch({type: 'add'})}>New TODO</button>
+    <br />
+    <br />
+    <TodosList items={state} />
+    </Context.Provider>
   );
 }
 
-export default App;
+function TodosList({items}) {
+  return  items.map(item=> <TodoItem key={item.id} {...item} />);
+}
+
+function TodoItem({id,completed,text}) {
+  const dispatch = useContext(Context);
+return <div
+  style={{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }}
+>
+  <input type="checkbox" checked={completed} onChange={() => dispatch({type: 'completed', payload: id})}/>
+  <input type="text" defaultValue={text} />
+  <button onClick={()=> dispatch({type:'delete', payload: id})}
+  >Delete
+  </button>
+  </div>;
+}
